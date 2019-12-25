2dc8e47c3e68ecf25924e7d9f0f2ebe4e69f3396
曹泽富 <zefucao@clubfactory.com>
2019-12-25 16:59:26 +0800

    update

--- src/views/user/settingDetail.vue
+++ src/views/user/settingDetail.vue
@@ -8,44 +8,89 @@
                 </template>
                 <img class="user-portrait" :src="userInfo.portrait" alt="">
             </van-cell>
-            <van-cell title="昵称" is-link :value="userInfo.nickname" @click="openPopup()"></van-cell>
+            <van-cell title="昵称" is-link :value="userInfo.nickname" @click="openPopup('昵称', userInfo.nickname, 'nickname')"></van-cell>
             <van-cell title="用户名" :value="userInfo.user_name"></van-cell>
-            <van-cell title="性别" is-link :value="userInfo.sex | formatSex"></van-cell>
-            <van-cell title="手机号" is-link :value="userInfo.phone"></van-cell>
+            <van-cell title="性别" is-link :value="userInfo.sex | formatSex" @click="openPopup('性别', userInfo.sex, 'sex')"></van-cell>
+            <van-cell title="手机号" is-link :value="userInfo.phone" @click="openPopup('手机号', userInfo.phone, 'phone')"></van-cell>
         </van-cell-group>
         <van-popup
                 v-model="show"
                 position="bottom"
-                :style="{ height: '20%' }"
+                :style="{ height: '18%', background: '#ededed' }"
         >
-            <van-cell-group>
+            <van-cell-group style="margin-top: 2vh;margin-bottom: 2vh">
                 <van-field
-                        value="123"
-                        label="用户名"
-                        left-icon="contact"
+                        v-model="newValue"
+                        :label="popupLabel"
+                        :placeholder="popupValue"
+                        clearable
+                        autofocus
                 ></van-field>
             </van-cell-group>
+            <div class="confirm-buttons">
+                <van-button type="primary" @click="confirm()">保存</van-button>
+                <van-button type="warning" @click="show = false">取消</van-button>
+            </div>
         </van-popup>
     </div>
 </template>
 
 <script>
     import {mapState} from 'vuex';
-    // import {Toast} from 'vant';
+    import {Toast} from 'vant';
 
     export default {
         name: "settingDetail",
         data() {
             return {
-                show: false
+                show: false,
+                popupLabel: '',
+                popupValue: '',
+                newValue: '',
+                type: ''
             }
         },
         methods: {
             back() {
                 this.$router.push('/setting');
             },
-            openPopup() {
+            openPopup(label, value, type) {
                 this.show = true;
+                this.popupLabel = label;
+                this.popupValue = value;
+                this.type = type;
+            },
+            confirm() {
+                if (!this.newValue) return;
+                this.$http.post('/api/updateUserInfo', {
+                    key: this.type,
+                    value: this.newValue
+                }).then(res => {
+                    if (res.data.code === 200) {
+                        Toast.success('修改成功');
+                        this.show = false;
+                        this.newValue = '';
+                        this.updateUserInfo()
+                    } else {
+                        Toast.fail(res.data.message);
+                    }
+                }).catch(() => {
+                    Toast.fail('修改失败，请稍后重试');
+                })
+            },
+            updateUserInfo() {
+                this.$http.get('/api/getUserInfo')
+                    .then(res => {
+                        if (res.data.code === 200) {
+                            this.$store.commit('login', res.data.data);
+                        } else {
+                            Toast.fail('获取用户信息失败');
+                            this.$store.commit('logout');
+                        }
+                    })
+                    .catch(() => {
+                        Toast.fail('获取用户信息失败');
+                    })
             }
         },
         computed: {
@@ -82,6 +127,13 @@
             float: right;
             width: 12vw;
             height: 12vw;
+        }
+        .confirm-buttons {
+            display: flex;
+            justify-content: space-around;
+            & > button {
+                width: 35vw;
+            }
         }
     }
 </style>
